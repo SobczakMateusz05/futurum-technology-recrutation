27e92a44e48ca090fc0c22e9ce145ad46dd99c70
SobczakMateusz05 <sobczak.mateusz27@gmail.com>
2025-07-26 22:41:00 +0200

    Upload some mocked data

---
+++ src/assets/data.ts
@@ -0,0 +1,12 @@
+// This file contains the mocked data for the application.
+// It is used to simulate API responses and provide data for showing purposes.
+// Normally, this file would be replaced with actual API endoints in a prdouction.
+
+export const userData = {
+    id: 1,
+    name: "John Doe",
+    email: "",
+    type: "Seller",
+};
+
+export const balanceData = 18532.45;
