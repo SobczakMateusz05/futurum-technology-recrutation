a1998da8f57b78a8458642b8c3698b4cf754e3be
SobczakMateusz05 <sobczak.mateusz27@gmail.com>
2025-07-26 22:42:25 +0200

    Start adding pages to root app file

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import NotFound from "./pages/NotFound/NotFound";
+
+function App() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
+    );
+}
+
+export default App;
