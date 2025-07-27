fcfb55e22bebe9188ca8e9bf2fa0ef010e340979
SobczakMateusz05 <sobczak.mateusz27@gmail.com>
2025-07-27 14:11:02 +0200

    Change main displaying

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import Topbar from "./components/Topbar/Topbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <Topbar />
+            <main>
+                <Sidebar />
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
         </>
     );
 }
