5e91cbebb4991b06256550872c09f19724edca99
SobczakMateusz05 <sobczak.mateusz27@gmail.com>
2025-07-28 17:23:46 +0200

    Change icon and link

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,14 +1,14 @@
-import ReactIcon from "../../assets/react.svg";
 import style from "./NotFound.module.scss";
-
+import ReactIcon from "../../assets/icons/react.svg";
+import { Link } from "react-router-dom";
 function NotFound() {
     return (
         <div className={style.container}>
             <h1 className={style.title}> 404</h1>
             <p className={style.message}> This page does not exist.</p>
-            <a href="/" className={style.link}>
+            <Link to="/" className={style.link}>
                 Go to main page
-            </a>
+            </Link>
             <img src={ReactIcon} alt="React Icon" className={style.icon} />
             {/* It's for demonstration in real app this logo will be changed with real app logo */}
             {/* This br and p won't exist in a real app */}
