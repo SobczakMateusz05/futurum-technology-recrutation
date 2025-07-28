385e3ad0e32cb4546e63e2bef07a7f6ce5ce0ccd
SobczakMateusz05 <sobczak.mateusz27@gmail.com>
2025-07-28 23:52:50 +0200

    Add responsivity to top line and add Campaign list

--- src/pages/Campaign/Campaign.tsx
+++ src/pages/Campaign/Campaign.tsx
@@ -2,16 +2,27 @@ import style from "./Campaign.module.scss";
 import Filter from "../../components/SearchFilters/Filter";
 import Search from "../../components/SearchFilters/Search";
 import CampaignToolbar from "../../components/CampaignToolbar/CampaignToolbar";
+import CampaignList from "../../components/CampaignList/CampaignList";
+import useBreakpoint from "../../hooks/useBreakpoint";
 
 function Campaign() {
+    const styles: React.CSSProperties | undefined =
+        useBreakpoint() === "mobile"
+            ? {
+                  flexDirection: "column-reverse",
+                  alignItems: "flex-start",
+              }
+            : undefined;
+
     return (
         <div className={style.container}>
-            <div className={style.topLine}>
+            <div className={style.topLine} style={styles}>
                 <Filter />
                 <Search />
             </div>
             <div className={style.content}>
                 <CampaignToolbar />
+                <CampaignList />
             </div>
         </div>
     );
