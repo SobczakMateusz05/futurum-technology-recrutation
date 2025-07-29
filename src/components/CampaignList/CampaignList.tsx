2c7408120df9aad535ed23396b7a08f5bbe6cd78
SobczakMateusz05 <sobczak.mateusz27@gmail.com>
2025-07-29 18:07:40 +0200

    Rebuild some features

--- src/components/CampaignList/CampaignList.tsx
+++ src/components/CampaignList/CampaignList.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import style from "./CampaignList.module.scss";
 import { EditIcon, DeleteIcon } from "../../assets/ComponentIcons";
-import DeleteConfirmation from "./DeleteConfirmation";
 
 import { campaignData } from "../../assets/data"; //This import is only for demonstration.
 
@@ -16,88 +15,107 @@ function CampaignList() {
         "status",
         "manage",
     ];
-    const [toDelete, setToDelete] = useState<number | null>(null);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleClick = (action: string, id: number) => {
+        searchParams.set("action", action);
+
+        searchParams.set("id", id.toString());
+        setSearchParams(searchParams);
+    };
 
     return (
-        <>
-            {toDelete !== null && (
-                <DeleteConfirmation
-                    id={toDelete}
-                    cancel={() => setToDelete(null)}
-                />
-            )}
-            <table className={style.container}>
-                <thead>
-                    <tr className={style.headerContainer}>
-                        {headers.map((title) => (
-                            <th className={style.header} key={title}>
-                                {title.toUpperCase()}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody className={style.contentContainer}>
-                    {campaignData.map((prop) => (
-                        <tr className={style.rowContainer} key={prop.id}>
-                            <td className={style.field}>{prop.name}</td>
-                            <td className={style.field}>
-                                {prop.keywords.map((keyword, i) =>
-                                    i < 3 ? (
-                                        <p className={style.keyword} key={i}>
-                                            {keyword}
-                                            {i < 2 ? ", " : ""}
-                                        </p>
-                                    ) : i === 3 ? (
-                                        <p className={style.keyword} key={i}>
-                                            , ...
-                                        </p>
-                                    ) : null
-                                )}
-                            </td>
-                            <td className={style.field}>
-                                {prop.bidAmount.toLocaleString("pl-PL", {
-                                    style: "currency",
-                                    minimumFractionDigits: 2,
-                                    maximumFractionDigits: 2,
-                                    currency: "PLN",
-                                })}
-                            </td>
-                            <td className={style.field}>
-                                {prop.fund.toLocaleString("pl-PL", {
-                                    style: "currency",
-                                    minimumFractionDigits: 2,
-                                    maximumFractionDigits: 2,
-                                    currency: "PLN",
-                                })}
-                            </td>
-                            <td className={style.field}> {prop.town} </td>
-                            <td className={style.field}>
-                                {prop.radius.toLocaleString("pl-PL", {
-                                    style: "unit",
-                                    minimumFractionDigits: 2,
-                                    maximumFractionDigits: 2,
-                                    unit: "kilometer",
-                                })}
-                            </td>
-                            <td className={style.field}>
-                                {prop.isActive ? "Active" : "Inactive"}
-                            </td>
-                            <td className={style.field}>
-                                <button className={style.button}>
-                                    <EditIcon className={style.icon} />
-                                </button>
-                                <button
-                                    className={style.button}
-                                    onClick={() => setToDelete(prop.id)}
-                                >
-                                    <DeleteIcon className={style.icon} />
-                                </button>
-                            </td>
-                        </tr>
+        <table className={style.container}>
+            <thead>
+                <tr className={style.headerContainer}>
+                    {headers.map((title) => (
+                        <th className={style.header} key={title}>
+                            {title.toUpperCase()}
+                        </th>
                     ))}
-                </tbody>
-            </table>
-        </>
+                </tr>
+            </thead>
+            <tbody className={style.contentContainer}>
+                {campaignData.map((prop) => (
+                    <tr
+                        className={style.rowContainer}
+                        key={prop.id}
+                        onClick={() => handleClick("focus", prop.id)}
+                    >
+                        <td className={style.field}>{prop.name}</td>
+                        <td className={style.field}>
+                            {prop.keywords.map((keyword, i) =>
+                                i < 3 ? (
+                                    <p className={style.keyword} key={i}>
+                                        {keyword}
+                                        {i < 2 ? ", " : ""}
+                                    </p>
+                                ) : i === 3 ? (
+                                    <p className={style.keyword} key={i}>
+                                        , ...
+                                    </p>
+                                ) : null
+                            )}
+                        </td>
+                        <td className={style.field}>
+                            {prop.bidAmount.toLocaleString("pl-PL", {
+                                style: "currency",
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                                currency: "PLN",
+                            })}
+                        </td>
+                        <td className={style.field}>
+                            {prop.fund.toLocaleString("pl-PL", {
+                                style: "currency",
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                                currency: "PLN",
+                            })}
+                        </td>
+                        <td className={style.field}> {prop.town} </td>
+                        <td className={style.field}>
+                            {prop.radius.toLocaleString("pl-PL", {
+                                style: "unit",
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                                unit: "kilometer",
+                            })}
+                        </td>
+                        <td
+                            className={style.field}
+                            style={
+                                prop.isActive
+                                    ? { color: "green" }
+                                    : { color: "red" }
+                            }
+                        >
+                            {prop.isActive ? "Active" : "Inactive"}
+                        </td>
+                        <td className={style.field}>
+                            <button
+                                className={style.button}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    handleClick("edit", prop.id);
+                                }}
+                            >
+                                <EditIcon className={style.icon} />
+                            </button>
+                            <button
+                                className={style.button}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    handleClick("delete", prop.id);
+                                }}
+                            >
+                                <DeleteIcon className={style.icon} />
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
 
