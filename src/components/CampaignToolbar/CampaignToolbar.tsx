1b3fb9602fb6bf1e54399c8ba152ea18ffaed6d8
SobczakMateusz05 <sobczak.mateusz27@gmail.com>
2025-07-29 18:07:55 +0200

    Create add button

--- src/components/CampaignToolbar/CampaignToolbar.tsx
+++ src/components/CampaignToolbar/CampaignToolbar.tsx
@@ -1,10 +1,18 @@
 import style from "./CampaignToolbar.module.scss";
 import Sort from "./Sort";
+import PlusIcon from "../../assets/icons/plus.svg";
+import { useSearchParams } from "react-router-dom";
 
 import { campaignData } from "../../assets/data"; //This import is only for demonstration.
 
 function CampaignToolbar() {
     const campaignAmount = campaignData.length;
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleAddButtonClick = () => {
+        searchParams.set("action", "add");
+        setSearchParams(searchParams);
+    };
 
     return (
         <div className={style.container}>
@@ -12,6 +20,9 @@ function CampaignToolbar() {
                 <p className={style.amount}>{campaignAmount} Campaigns</p>
                 <Sort />
             </div>
+            <button className={style.button} onClick={handleAddButtonClick}>
+                <img src={PlusIcon} alt="Plus icon" className={style.icon} />
+            </button>
         </div>
     );
 }
